f6016227678aeaa8e4a550b6d4183fb070bc0e0c
Dharmesh Vasani <dharmesh.vasani@multidots.in>
2017-11-22 15:42:24 +0530

    post crud operation

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,12 @@ Route::get('foo', function () {
 Route::get('user/{id}', function ($id) {
     return 'User '.$id;
 });
+
+
+Route::get('/posts', 'PostsController@index')->name('posts.index');
+Route::get('/posts/details/{id}', 'PostsController@details')->name('posts.details');
+Route::get('/posts/add', 'PostsController@add')->name('posts.add');
+Route::post('/posts/insert', 'PostsController@insert')->name('posts.insert');
+Route::get('/posts/edit/{id}', 'PostsController@edit')->name('posts.edit');
+Route::post('/posts/update/{id}', 'PostsController@update')->name('posts.update');
+Route::get('/posts/delete/{id}', 'PostsController@delete')->name('posts.delete');
